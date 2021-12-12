762d4abc9201d57961c8bb4cdc082afc7281b6d7
Jimmy Wong <tszming.wong@gmail.com>
2021-12-12 16:23:31 +0800

    Test Route 53

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "url" {
-  value = cloudflare_record.www.hostname
-}
+# output "url" {
+#   value = cloudflare_record.www.hostname
+# }
 
 output "db_hostname" {
   value = module.rds.db_instance_address
