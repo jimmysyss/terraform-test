d8a2919a11d8091d01fb8b9f63306b365800c1c0
Jimmy Wong <tszming.wong@gmail.com>
2021-12-14 13:57:12 +0800

    Test Cloudfront

--- 20_cloudfront.tf
+++ 20_cloudfront.tf
@@ -28,10 +28,9 @@ module "cloudfront" {
     appsync = {
       domain_name = module.alb.lb_dns_name
       custom_origin_config = {
-        http_port              = 80
         https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        origin_protocol_policy = "https-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
       }
 
       custom_header = [
@@ -64,13 +63,13 @@ module "cloudfront" {
     group_one = {
       failover_status_codes      = [403, 404, 500, 502]
       primary_member_origin_id   = "appsync"
-      secondary_member_origin_id = "s3_one"
+      # secondary_member_origin_id = "s3_one"
     }
   }
 
   default_cache_behavior = {
     target_origin_id       = "appsync"
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
     compress               = true
