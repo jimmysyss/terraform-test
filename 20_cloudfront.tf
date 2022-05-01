52799f75f2b093af7147c9461c3d751e32a23333
Jimmy Wong <tszming.wong@gmail.com>
2022-05-02 00:41:25 +0800

    Test Cloudfront 03

--- 20_cloudfront.tf
+++ 20_cloudfront.tf
@@ -102,6 +102,9 @@ module "cloudfront" {
     cached_methods         = ["GET", "HEAD"]
     compress               = true
     query_string           = true
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
     #cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     #response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
   }]
